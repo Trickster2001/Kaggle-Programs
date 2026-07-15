f34ec6344284193ed563ee8968bb93d5baa1ded2
Trickster2001 <keyurmurkar@gmail.com>
2026-07-15 13:27:13 +0530

    Conditions Programs

--- Python Foundations.py
+++ Python Foundations.py
@@ -137,3 +137,114 @@ def averageMarksCalculator(myArr):
     if not myArr:
         return 0.0
     return sum(myArr) / len(myArr)
+
+
+## Conditions
+# Even or Odd
+myNum = input("Enter the number")
+if myNum%2==0:
+    print("even")
+else:
+    print("odd")
+
+# Leap Year
+def isLeap(year):
+    if (year%4==0 and year%100!=0) or year%400 ==0:
+        return "Leap"
+    
+# Grade Calculator
+if myNum >= 90:
+    print("A grade")
+elif myNum >= 70:
+    print("B grade")
+elif myNum >= 50:
+    print("C grade")
+else:
+    print("F grade")
+
+# ATM Withdrawl
+def atmWithdrawl(req_amt, rem_amt, atm_avail_cash, daily_limit):
+    if req_amt<=0:
+        return "Invalid amt"
+    if req_amt%10!=0:
+        return "req amt must be multiple of 10"
+    if req_amt>daily_limit or req_amt>atm_avail_cash or req_amt>rem_amt:
+        return "req amt must be less"
+    
+    new_user_balance = rem_amt - req_amt
+    new_atm_cash = atm_avail_cash - req_amt
+    new_daily_limit = daily_limit - req_amt
+    
+    return {
+        "status": "Success",
+        "dispensed": req_amt,
+        "remaining_account_balance": new_user_balance,
+        "atm_vault_balance": new_atm_cash,
+        "daily_limit_left": new_daily_limit
+    }
+
+# Voting Eligibility
+if myNum<18:
+    print("Not Eligible")
+else:
+    print("Eligible")
+
+# Largest Of Three Numbers
+a = input("Enter first number")
+b = input("Enter second number")
+c = input("Enter third number")
+
+if a>=b and a>=c:
+    print("a is largest")
+elif b>=a and b>=c:
+    print("b is largest")
+elif c>=a and c>=b:
+    print("c is largest")
+
+# Traingle Type
+def isTriangleType(a,b,c):
+    if (a+b<=c) or (b+c<=a) or (c+a<=b):
+        return "Not a triangle"
+    
+    if a==b==c:
+        return "Equilateral Triangle"
+    elif a==b or b==c or c==a:
+        return "Isosceles Triangle"
+    else:
+        return "Scalene Triangle"
+    
+# Quadratic Function
+def quadFunction(a,b,c):
+    if a==0:
+        return "Not a quadratic function"
+    
+    discriminant = (b*b) - (4*a*c)
+
+    if discriminant > 0:
+        return "Two distinct real roots"
+    elif discriminant == 0:
+        return "One real root"
+    else:
+        return "Two complex roots"
+    
+# Traffic Light
+def trafficLight(color):
+    if color=="Red":
+        return "Stop"
+    elif color == "Orange":
+        return "Stop"
+    else:
+        return "Go"
+
+# Rock Paper Scissor
+def rockPaperScissor(p1, p2):
+    if p1 == p2:
+        return "Nothing"
+    elif p1== "rock" and p2=="paper":
+        return "P2 Wins"
+    elif p1=="paper" and p2=="scissor":
+        return "P2 Wins"
+    elif p1 == "scissor" and p2 == "rock":
+        return "P2 Wins"
+    else:
+        return "P1 Wins"
